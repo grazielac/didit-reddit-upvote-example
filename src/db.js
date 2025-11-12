2cd7334d564214275d12613185128eaa42b4dd35
grazielac <cpgraziela@gmail.com>
2025-11-12 16:28:40 +0000

    Change db url

--- src/db.js
+++ src/db.js
@@ -2,6 +2,7 @@ import { Pool } from "pg";
 
 export const db = new Pool({
   connectionString: process.env.DATABASE_URL,
+  host: db.lmpxfnnvtgrqredngjug.supabase.co,
   ssl: { rejectUnauthorized: false },
   family: 4,
 });
