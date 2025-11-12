fd8ff5082391c43a6a8eacff9983ce57eb46b8f5
grazielac <cpgraziela@gmail.com>
2025-11-12 16:46:24 +0000

    remove host

--- src/db.js
+++ src/db.js
@@ -2,7 +2,6 @@ import { Pool } from "pg";
 
 export const db = new Pool({
   connectionString: process.env.DATABASE_URL,
-  host: db.lmpxfnnvtgrqredngjug.supabase.co,
   ssl: { rejectUnauthorized: false },
   family: 4,
 });
